01a17fd8cae634b324653e00929d5cd1ff52a6b7
mmeyers-solartech <mmeyers@solartechnology.com>
2020-05-01 16:46:39 -0400

    Updates react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import JsmpegPlayer from './components/JsmpegPlayer';
+
+const videoOptions = {
+  poster: 'https://cycjimmy.github.io/staticFiles/images/screenshot/big_buck_bunny_640x360.jpg'
+};
+
+const videoOverlayOptions = {autoplay: true};
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <JsmpegPlayer
+          wrapperClassName="video-wrapper"
+          videoUrl="ws://127.0.0.1:9999"
+          options={videoOptions}
+          overlayOptions={videoOverlayOptions}
+        />
       </header>
     </div>
   );
